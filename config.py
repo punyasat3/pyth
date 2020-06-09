6ff51358a24b5eb1d5375a8a4df7a732d4ac965d
punyasat3 <59074514+punyasat3@users.noreply.github.com>
2020-06-09 16:21:02 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+access_key="AKIARMDSSSFUTGZ3XH7D"
+secret_access_key="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"
+regions=["us-east-1","ap-south-1"]
+ec2_tag_name=["pythonp","python","server"]
+availablity_zone=["us-east-1e","ap-south-1a"]
+elb_names=["celb2","celb"]
