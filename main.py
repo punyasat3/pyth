3d1be026714b1ccd38ed1365d249f14ca1afa3ab
punyasat3 <59074514+punyasat3@users.noreply.github.com>
2020-06-11 17:07:44 +0530

    Update main.py
    deregister script with logginfo

--- main.py
+++ main.py
@@ -6,14 +6,17 @@ import sys
 import logging
 from config import *
 
+logging.basicConfig(filename='elb.log', level=logging.INFO,
+                    format='%(asctime)s:%(levelname)s:%(message)s')
+
 default_region="ap-south-1"
 #Credentials Validation
 sts = boto3.client('sts',aws_access_key_id=access_key,aws_secret_access_key=secret_access_key,region_name=default_region)
 try:
     sts.get_caller_identity()
-    print("Credentials are valid.")
+    logging.info("Credentials are valid.")
 except:
-    print("Credentials invalid. Please Provide Valid Credentials in config file.Thanks")
+    logging.info("Credentials invalid. \nPlease Provide Valid Credentials in config file.\nThanks")
     sys.exit(0)
 
 #Region Validation
@@ -21,32 +24,19 @@ client = boto3.client('ec2',aws_access_key_id=access_key,aws_secret_access_key=s
 regions_list = [region['RegionName'] for region in client.describe_regions()['Regions']]
 for region in regions:
     if region not in regions_list:
-        print("{region} No Such Region Found.Please Enter Valid Region in config file.Thanks")
+        logging.info(region,"No Such Region Found.\nPlease Enter Valid Region in config file.\nThanks")
+        logging.debug("please select mention proper region in provided by AWS")
         sys.exit(1)
 
 #Checks
 if len(regions)==0:
-    print("Please Enter Atleast One region in Config file.")
+    logging.info("Please Enter Atleast One region in Config file.")
     sys.exit(2)
 if len(ec2_tag_names)==0:
-    print("Please Enter Atleast One EC2 Tag name in Config file.")
+    logging.info("Please Enter Atleast One EC2 Tag name in Config file.")
     sys.exit(3)
 
 instance_ids_with_matching_tags=[]
-for region in regions:
-    client = boto3.client('ec2',aws_access_key_id=access_key,aws_secret_access_key=secret_access_key,region_name=region)
-    response = client.describe_instances(Filters=ec2_tag_names)
-    for d in response['Reservations']:
-        for z in d['Instances']:
-            instance_ids_with_matching_tags.append(z['InstanceId'])
-if len(instance_ids_with_matching_tags)==0:
-    print("No matching instances found in this tagging creteria. Thanks")
-    sys.exit(4)
-# print(instance_ids_with_matching_tags)
-
-elb_list=[]
-instance_list=[]
-# Fetching all available ELBs names from this account.
 
 for region in regions:
     client = boto3.client('ec2',aws_access_key_id=access_key,aws_secret_access_key=secret_access_key,region_name=region)
@@ -55,7 +45,7 @@ for region in regions:
         for z in d['Instances']:
             instance_ids_with_matching_tags.append(z['InstanceId'])
 if len(instance_ids_with_matching_tags)==0:
-    print("No matching instances found in this tagging creteria. Thanks")
+    logging.info("No matching instances found in this tagging creteria. \nPlease Provide Valid Tags.\nThanks")
     sys.exit(4)
 # print(instance_ids_with_matching_tags)
 
@@ -69,7 +59,7 @@ for region in regions:
     for z in a:
         elb_list.append(z['LoadBalancerName'])
 elb_names=elb_list
-print("Trying to Deregister Instances...")
+logging.info("Trying to Deregister Instances...")
 
 deregistered_instances=[]
 instance_list=[]
@@ -93,7 +83,7 @@ for region in regions:
                                 instance_list.append(t['InstanceId'])
                                 for elb in new_list:
                                     elb_response = elb_client.deregister_instances_from_load_balancer(LoadBalancerName=elb,Instances=[{'InstanceId': t['InstanceId']}])
-                                    print(t['InstanceId'],"is Deregistered from ELB named ",elb)
+                                    logging.warning(str(t['InstanceId'])+" is Deregistered from ELB named "+str(elb)+" in region "+str(region))
 # print(list(dict.fromkeys(instance_list)))
 if len(instance_list)==0 :
-    print("Nothing to DeregisterThanks.")
+    logging.info("Nothing to Deregister.\nThanks)
