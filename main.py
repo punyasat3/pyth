4bb4c2b496f67c693f391675da2274c417042dbc
punyasat3 <59074514+punyasat3@users.noreply.github.com>
2020-06-15 13:14:38 +0530

    Update main.py
    script with multi thread

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 #! /usr/local/bin/python3
 
 import os
+import threading
 import boto3
 import sys
 import logging
+from time import sleep
 from config import *
 
 logging.basicConfig(filename='elb.log', level=logging.INFO,
@@ -14,7 +16,7 @@ default_region="ap-south-1"
 sts = boto3.client('sts',aws_access_key_id=access_key,aws_secret_access_key=secret_access_key,region_name=default_region)
 try:
     sts.get_caller_identity()
-    logging.warning("Credentials are valid.")
+    logging.info("Credentials are valid.")
 except:
     logging.info("Credentials invalid. \nPlease Provide Valid Credentials in config file.\nThanks")
     sys.exit(0)
@@ -24,8 +26,8 @@ client = boto3.client('ec2',aws_access_key_id=access_key,aws_secret_access_key=s
 regions_list = [region['RegionName'] for region in client.describe_regions()['Regions']]
 for region in regions:
     if region not in regions_list:
-        logging.info(str(region)+" No Such Region Found.\nPlease Enter Valid Region in config file.\nThanks")
-        logging.debug("please check aws regions here "+str(region_list))
+        logging.info(region,"No Such Region Found.\nPlease Enter Valid Region in config file.\nThanks")
+        logging.debug("please select mention proper region in provided by AWS")
         sys.exit(1)
 
 #Checks
@@ -37,7 +39,6 @@ if len(ec2_tag_names)==0:
     sys.exit(3)
 
 instance_ids_with_matching_tags=[]
-
 for region in regions:
     client = boto3.client('ec2',aws_access_key_id=access_key,aws_secret_access_key=secret_access_key,region_name=region)
     response = client.describe_instances(Filters=ec2_tag_names)
@@ -63,7 +64,8 @@ logging.info("Trying to Deregister Instances...")
 
 deregistered_instances=[]
 instance_list=[]
-for region in regions:
+
+def deregister(region):
     new_list=[]
     elb_client = boto3.client('elb',aws_access_key_id=access_key,aws_secret_access_key=secret_access_key,region_name=region)
     elb_response=elb_client.describe_load_balancers()
@@ -84,6 +86,10 @@ for region in regions:
                                 for elb in new_list:
                                     elb_response = elb_client.deregister_instances_from_load_balancer(LoadBalancerName=elb,Instances=[{'InstanceId': t['InstanceId']}])
                                     logging.warning(str(t['InstanceId'])+" is Deregistered from ELB named "+str(elb)+" in region "+str(region))
+
+for region in regions:
+    t = threading.Thread(target = deregister, args=(region,)).start()
+
 # print(list(dict.fromkeys(instance_list)))
 if len(instance_list)==0 :
-    logging.info("Nothing to Deregister.\nThanks)
+    logging.info("Nothing to Deregister.\nThanks.")
