4ce24088547b9c0d2e719516e3548a676c37944d
punyasat3 <59074514+punyasat3@users.noreply.github.com>
2020-07-19 13:09:50 +0530

    code with role based

--- config.py
+++ config.py
@@ -1,9 +1,17 @@
-access_key="onsjdj"
-secret_access_key="/Qz3TCL3bgZdxCm45Dn"
+access_key="AKIAR"
+secret_access_key="v2NCYHm4vEyCUX"
+Account_Id="094729572713"
+Account_Name="punya2000"
 regions=["ap-south-1","us-east-1"]
-ec2_tag_names=[{'Name':'tag:deregister', 'Values':['true']},{'Name':'tag:environment', 'Values':['production']}]
+ec2_tag_names=[{'Name':'tag:Deregister', 'Values':['true']},{'Name':'tag:Environment', 'Values':['production']}]
 Level=20
 default_region="ap-south-1"
+Instance_Deregistration_checkingtime=30
+Maxattempts=12
+AfterderegisterChangingKey='Deregister'
+AfterderegisterChangingValue='false'
+HealthStatus="unhealthy"
+Specifictag='Role'
 #CRITICA 50
 #ERROR  40
 #WARNIN 30
