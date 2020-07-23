e730e5986f98aa8a4be98601f141436fb78660ce
punyasat3 <59074514+punyasat3@users.noreply.github.com>
2020-07-23 10:18:37 +0530

    with all changes from sk ak

--- config.py
+++ config.py
@@ -1,9 +1,18 @@
-access_key="onsjdj"
-secret_access_key="/Qz3TCL3bgZdxCm45Dn"
+access_key="KIARMDSSSFUU2AVMHU5"
+secret_access_key="2NCYHm4vEyCUXdzgrAOW/Qz3TCL3bgZdxCm45Dn"
+Account_Id="094729572713"
+Account_Name="punya2000"
 regions=["ap-south-1","us-east-1"]
-ec2_tag_names=[{'Name':'tag:deregister', 'Values':['true']},{'Name':'tag:environment', 'Values':['production']}]
+ec2_tag_names=[{'Name':'tag:Deregister', 'Values':['true']},{'Name':'tag:Environment', 'Values':['production']}]
 Level=20
+role="s3_cross_account"
 default_region="ap-south-1"
+Instance_Deregistration_checkingtime=30
+Maxattempts=12
+AfterderegisterChangingKey='Deregister'
+AfterderegisterChangingValue='false'
+HealthStatus="unhealthy"
+Specifictag='Role'
 #CRITICA 50
 #ERROR  40
 #WARNIN 30
