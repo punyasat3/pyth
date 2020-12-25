ea6d81c4475896dee165ac3e9983f064f2f6345d
punyasat3 <59074514+punyasat3@users.noreply.github.com>
2020-12-25 19:55:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from config import *
 #log file
 import slackweb
 
-slack = slackweb.Slack(url=""https://hooks.slack.com/services/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"")
+slack = slackweb.Slack(url="https://hooks.")
 def slack_notify(script_title,channel_name,attachments,severity):
         if (severity =="info"):
                 color = "#439FE0"
